bab4ea476af5c6c90eba61e6bbcfd6b9af660a84
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WpfApplication1/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DevExpress.Xpo;
+using System.Windows;
+using System.ComponentModel;
+
+namespace WpfApplication1 {
+    public class ContactItem {
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+    }
+
+}
